d27bfd94bf1320a65546c4fea8b1b5cfbba46ff5
bohdan <57547705+r0ugeEngine@users.noreply.github.com>
2022-07-26 14:54:25 +0300

    Update TicTacToe.sol

--- TicTacToe.sol
+++ TicTacToe.sol
@@ -1,66 +1,103 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.15;
 
 import "hardhat/console.sol";
 
 contract TicTacToe {
     //adresses of players and whose turn is to do action
-    address playerX;
-    address playerO;
-    address whosAction;
-    //matrix of play zone
-    uint[9] public playZone;
-    //flag to check if players have changed
-    bool playerSwitched = false;
+    address internal playerX;
+    address internal playerO;
+    address internal whosAction;
+    //matrix of the play field
+    uint[9] internal playField;
 
-    //modifier to check that exactly two players are playing
-    modifier playersSelected() {
+    //modifier to check that exactly two players are playing and to check that players are switched
+    modifier playersChecker {
         require(msg.sender == playerX || msg.sender == playerO, "Only players can call this function");
+        require(msg.sender == whosAction, "You cannot do this action until your opponent has chosen an action!");
+        _;
+    }
+    
+
+    //modifier to check game over
+    modifier notGameOver {
+        require(!gameOver(), "Game Over!");
         _;
     }
 
+    //modifier to check for empty cells
+    modifier zoneChecker(uint position) {
+        require(playField[position] == 0, "Choose another position!");
+        _;
+    }
+
+    //smart contract constructor, called at the beginning
     constructor(address _playerO) {
         playerX = msg.sender;
         playerO = _playerO;
         whosAction = playerX;
-        console.log(whosAction);
     }
 
-    function setAction(uint position) external playersSelected() {
-        require(msg.sender == whosAction, "You cannot do this action until your opponent has chosen an action!");
-        console.log(whosAction);
+    //sets the sign of the X and O, on the field
+    function setAction(uint position) external notGameOver playersChecker zoneChecker(position) {
+        uint8[2] memory playersSigns = [1,2];
+        uint signInArray = 0;
+
         if(whosAction == playerX) {
-            zoneChecker(position, 2);
-            playZone[position] = 1;
             whosAction = playerO;
-            console.log(whosAction);
         } else {
-            zoneChecker(position, 1);
-            playZone[position] = 2;
+            signInArray++;
             whosAction = playerX;
-            console.log(whosAction);
         }
+
+        playField[position] = playersSigns[signInArray];
     }
 
 
-    function showZone() public view returns(string memory, string memory, string memory) {
-        bytes memory zone = new bytes(9);
+    //shows game field
+    function showField() external view returns(string memory, string memory, string memory) {
+        bytes memory field = new bytes(9);
         bytes1[3] memory symbols = [bytes1("_"), bytes1("X"), bytes1("O")];
 
-        for(uint i = 0; i < zone.length; ++i) {
-            zone[i] = symbols[playZone[i]];  
+        for(uint i = 0; i < field.length; ++i) {
+            field[i] = symbols[playField[i]];  
         }
-        string memory one = string(bytes.concat(zone[0],zone[1],zone[2]));
+
+        string memory one = string(bytes.concat(field[0],field[1],field[2]));
         one = string.concat(one, "\n");
-        string memory two = string(bytes.concat(zone[3],zone[4],zone[5]));
+        string memory two = string(bytes.concat(field[3],field[4],field[5]));
         two = string.concat(two, "\n");
-        string memory three = string(bytes.concat(zone[6],zone[7],zone[8]));
+        string memory three = string(bytes.concat(field[6],field[7],field[8]));
         three = string.concat(three, "\n");
 
         return(one, two, three);
     }
 
-    function zoneChecker(uint position, uint chosenZones) internal view {
-        require(playZone[position] != chosenZones, "Choose another position because your opponent chose it!");
+    //checks if the game is over
+    function gameOver() public view returns(bool) {
+        //check rows
+        for(uint i = 0; i < playField.length - 3; i+=3) {
+            if(winDefinition(i,i+1,i+2))
+                return true;
+        }
+
+        //check columns
+        for(uint i = 0; i < playField.length - 6; i++) {
+            if(winDefinition(i,i+3,i+6))
+                return true;
+        }
+
+        //check diagonals
+        if(winDefinition(0, 4, 8))
+            return true;
+        if(winDefinition(2, 4, 6))
+            return true;
+
+        return false;
+    }
+    
+    //ways to win
+    function winDefinition(uint positionOne, uint positionTwo, uint positionThree) internal view returns(bool) {
+        return playField[positionOne] != 0 && playField[positionOne] == playField[positionTwo] && playField[positionTwo] == playField[positionThree];
     }
 }
